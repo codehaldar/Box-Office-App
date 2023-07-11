ac103e402ca9e8e8657c93749a4e9f60d0b52988
codehaldar <subhamhaldar2001@gmail.com>
2023-07-11 17:55:14 +0530

    added error handler, apiData state and apiDataError state added

---
+++ src/api/tvmaze.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://api.tvmaze.com';
+const Getapi = async queryString => {
+  //throw new Error('Something is Wrong');
+  const response = await fetch(`${BASE_URL}${queryString}`);
+  const body = await response.json();
+  return body;
+};
+export const searchForShows = query => Getapi(`/search/shows?q=${query}`);
