ace7ab3b243d62b9655000d768dab9994d10f951
codehaldar <subhamhaldar2001@gmail.com>
2023-07-20 23:13:35 +0530

    fetched and displayed starred show data on starred page

--- src/Pages/Starred.jsx
+++ src/Pages/Starred.jsx
@@ -1,7 +1,26 @@
 import { useStarredShows } from '../lib/useStarredShows';
-
+import { getShowsByIds } from '../api/tvmaze';
+import { useQuery } from 'react-query';
+import ShowGrid from '../components/shows/ShowGrid';
 const Starred = () => {
-  const [starredShows] = useStarredShows();
-  return <div>Starred Shows, Starred {starredShows.length} </div>;
+  const [starredShowsIds] = useStarredShows();
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowsByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+  if (starredShows?.length === 0) {
+    return <div>NO SHOWS ARE STARRED</div>;
+  }
+  if (starredShowsError) {
+    return <div> Error Found: {starredShowsError.message}</div>;
+  }
+  return <div>Starred shows are loading</div>;
 };
 export default Starred;
--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -9,3 +9,8 @@ export const searchForShows = query => Getapi(`/search/shows?q=${query}`);
 export const searchForPeople = query => Getapi(`/search/people?q=${query}`);
 export const getShowById = ShowId =>
   Getapi(`/shows/${ShowId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsByIds = async ShowIds => {
+  const requestPromises = ShowIds.map(id => Getapi(`/shows/${id}`));
+  return await Promise.all(requestPromises);
+};
